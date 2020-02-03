5ec25f21a80bb029d08e52c3543a9dc5896f699d
mugendinjue <mugendinjue7@gmail.com>
2020-02-04 00:33:01 +0300

    make minor small changes such as the theme

--- netify/requests.py
+++ netify/requests.py
@@ -13,8 +13,26 @@ movie_url ='https://api.themoviedb.org/3/movie/{}?api_key={}'
 
 movie_trailer='https://api.themoviedb.org/3/movie/{}/videos?api_key={}'
 
+# Get a movies details
+
+movie_details_url='https://api.themoviedb.org/3/movie/{}?api_key={}'
+
+# imdb (internet movie database):
+
+imdb_url = "https://movie-database-imdb-alternative.p.rapidapi.com/"
+
+# Get similar movie to the searched one
+
+similar_movies_url='https://api.themoviedb.org/3/movie/{}/similar?api_key={}'
+
+# Get movie reviews
+
+movie_review_url='https://api.themoviedb.org/3/movie/{}/reviews?api_key={}'
+
 # The youtube url
+
 # plays the video id
+
 youtube='https://www.googleapis.com/youtube/v3/search?part=snippet&q={}&key={}'
 
 # The API keys
@@ -35,6 +53,14 @@ class Movies():
     self.vote_average = vote_average
     self.overview = overview
     self.release_date = release_date
+
+
+class Reviews:
+  def __init__(self,author,content,url):
+    self.author = author
+    self.content = content
+    self.url = url
+
   
     
 # The function that sends the request to movie database api
@@ -61,12 +87,77 @@ def getMovies(category):
     list.append(movie_object)
   return list
 
-# functtion that get the first trailer id
+# function that get the first trailer id
 def trailer_id(movie_id):
 
-  trailers = movie_trailer.format(int(movie_id),api_key)
-  response = requests.get(trailers)
+  trailers_url = movie_trailer.format(int(movie_id),api_key)
+  response = requests.get(trailers_url)
   data = response.json()
   the_id = data['results'][0].get('key')
 
   return the_id
+
+# function that gets a specific movies details
+def movie_details(movie_id):
+
+  details_url = movie_details_url.format(movie_id,api_key)
+  response = requests.get(details_url)
+  data = response.json()
+
+  return data
+
+# function that contacts imdb
+# Not really necessary...info is almost simmilar as that from novie database
+# Usefull if added info is required
+
+def imdb(imdb_id):
+
+  querystring = {"i":imdb_id,"r":"json"}
+
+  headers = {
+      'x-rapidapi-host': "movie-database-imdb-alternative.p.rapidapi.com",
+      'x-rapidapi-key': config('x-rapidapi-key')
+      }
+
+  response = requests.request("GET", imdb_url, headers=headers, params=querystring)
+
+  return response
+
+# search similar movies
+def get_similar_movies(movie_id):
+  complete_url = similar_movies_url.format(movie_id,api_key)
+  response = requests.get(complete_url)
+  movies = response.json()
+  
+
+  list = []
+  for new_movie in movies['results']:
+    popularity = new_movie.get('popularity')
+    vote_count = new_movie.get('vote_count')
+    poster_path = new_movie.get('poster_path')
+    id = new_movie.get('id')
+    original_language = new_movie.get('original_language')
+    original_title = new_movie.get('original_title')
+    vote_average = new_movie.get('vote_average')
+    overview = new_movie.get('overview')
+    release_date = new_movie.get('release_date')
+
+    movie_object = Movies(popularity,vote_count,poster_path,id,original_language,original_title,vote_average,overview,release_date)
+    list.append(movie_object)
+  return list
+  
+def get_movie_reviews(movie_id):
+  complete_url = movie_review_url.format(movie_id,api_key)
+  response = requests.get(complete_url)
+  data = response.json()
+
+  reviews = []
+  for review in data['results']:
+    author = review.get('author')
+    content = review.get('content')
+    url = review.get('url')
+
+    new_review = Reviews(author = author, content = content, url = url)
+    reviews.append(new_review)
+
+  return reviews
--- netify/views.py
+++ netify/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
-from .requests import getMovies,trailer_id
+from .requests import getMovies,trailer_id,movie_details,imdb,get_similar_movies,get_movie_reviews
+
+
+
+# companies object
+
+class Company:
+  def __init__(self,name,logo,origin):
+    self.name = name
+    self.logo = logo
+    self.origin = origin
 
 # The home view function with movie posters
 
@@ -22,9 +32,58 @@ def home(request):
 
 def tube(request,movie_id):
   
-  youtube_video_id = trailer_id(movie_id)
+  try:
+    the_movie_details = movie_details(movie_id)
+  except:
+    the_movie_details = 'Service not available at the moment'
+
+  try:
+    youtube_video_id = trailer_id(movie_id)
+  except:
+    youtube_video_id = 'Service not available at the moment'
+
+  try:
+    similar_movies = get_similar_movies(movie_id)
+  except:
+    similar_movies = 'Service not available at the moment'
+
+  try:
+    movie_reviews = get_movie_reviews(movie_id)
+  except:
+    movie_reviews = 'Service not available at the moment'
+
+  all_genres = []
+  production_countries = []
+  production_companies_name = []
+
+  genre = the_movie_details['genres']
+  countries = the_movie_details['production_countries']
+  companies = the_movie_details['production_companies']
+  
+  for g in genre:
+    name = g.get('name')
+    all_genres.append(name)
+
+  for p in countries:
+    name = p.get('name')
+    production_countries.append(name)
+
+  for c in companies:
+    name = c.get('name')
+    logo = c.get('logo_path')
+    origin = c.get('origin_country')
+    company_object = Company(name = name, logo = logo, origin = origin)
+    production_companies_name.append(company_object)
+  print(movie_reviews)
   context = {
+
     'youtube_video_id':youtube_video_id,
+    'the_movie_details':the_movie_details,
+    'all_genres':all_genres,
+    'production_countries':production_countries,
+    'production_companies_name':production_companies_name,
+    'similar_movies':similar_movies,
+    'movie_reviews':movie_reviews,
   }
 
   return render(request,'test.html',context)
