209d9ae8b2daef22fbf60316dea43d671a97bae5
mugendinjue <mugendinjue7@gmail.com>
2020-02-07 12:00:12 +0300

    remove try except from view module

--- netify/views.py
+++ netify/views.py
@@ -11,35 +11,30 @@ movie_search = 'movie'
 tv_search = 'tv'
 
 def search(request):
+  if 'search_term' in request.GET and request.GET["search_term"]:
+    search_name = request.GET.get('search_term')
+    found_movies = searchMovies(movie_search_url,movie_search,search_name)
+    found_series = searchSeries(movie_search_url,tv_search,search_name)
 
-  try:
-    if 'search_term' in request.GET and request.GET["search_term"]:
-      search_name = request.GET.get('search_term')
-      found_movies = searchMovies(movie_search_url,movie_search,search_name)
-      found_series = searchSeries(movie_search_url,tv_search,search_name)
+    context = {
+      'found_movies':found_movies,
+      'found_series':found_series,
+      'search_name':search_name
+    }
 
-      context = {
-        'found_movies':found_movies,
-        'found_series':found_series,
-        'search_name':search_name
-      }
+    return render(request,'search.html',context)
+  else:
+    return redirect(index)
 
-      return render(request,'search.html',context)
-    else:
-      return redirect(index)
-  except:
-    return HttpResponse('There seems to be no internet connection')
 
 
 def index(request):
-  try:
-    context = {
+  context = {
 
-    }
+  }
 
-    return render(request,'index.html',context)
-  except:
-    return HttpResponse('There seems to be no internet connection')
+  return render(request,'index.html',context)
+
 
 # MOVIES
 
@@ -48,85 +43,83 @@ def index(request):
 # The home view function with movie posters
 
 def home(request):
-  try:
-    top_rated_movies = getMovies('top_rated')
-    now_playing_movies = getMovies('now_playing')
-    upcoming_movies = getMovies('upcoming')
-    popular_movies = getMovies('popular')
 
-    context = {
-      "top_rated_movies":top_rated_movies,
-      "now_playing_movies":now_playing_movies,
-      "upcoming_movies":upcoming_movies,
-      "popular_movies":popular_movies,
-    }
+  top_rated_movies = getMovies('top_rated')
+  now_playing_movies = getMovies('now_playing')
+  upcoming_movies = getMovies('upcoming')
+  popular_movies = getMovies('popular')
 
-    return render(request,'home.html',context)
-  except:
-    return HttpResponse('There seems to be no internet connection')
+  context = {
+    "top_rated_movies":top_rated_movies,
+    "now_playing_movies":now_playing_movies,
+    "upcoming_movies":upcoming_movies,
+    "popular_movies":popular_movies,
+  }
+
+  return render(request,'home.html',context)
+
 
 
 #  view function with youtube video player
 
 def tube(request,movie_id):
+
   try:
-    try:
-      the_movie_details = movie_details(movie_id)
-    except:
-      the_movie_details = 'Service not available at the moment'
-
-    try:
-      youtube_video_id = trailer_id(movie_id)
-    except:
-      youtube_video_id = 'Service not available at the moment'
-
-    try:
-      similar_movies = get_similar_movies(movie_id)
-    except:
-      similar_movies = 'Service not available at the moment'
-
-    try:
-      movie_reviews = get_movie_reviews(movie_id)
-    except:
-      movie_reviews = 'Service not available at the moment'
-
-    all_genres = []
-    production_countries = []
-    production_companies_name = []
-
-    genre = the_movie_details['genres']
-    countries = the_movie_details['production_countries']
-    companies = the_movie_details['production_companies']
-    
-    for g in genre:
-      name = g.get('name')
-      all_genres.append(name)
-
-    for p in countries:
-      name = p.get('name')
-      production_countries.append(name)
-
-    for c in companies:
-      name = c.get('name')
-      logo = c.get('logo_path')
-      origin = c.get('origin_country')
-      company_object = Company(name = name, logo = logo, origin = origin)
-      production_companies_name.append(company_object)
-
-    context = {
-
-      'youtube_video_id':youtube_video_id,
-      'the_movie_details':the_movie_details,
-      'all_genres':all_genres,
-      'production_countries':production_countries,
-      'production_companies_name':production_companies_name,
-      'similar_movies':similar_movies,
-      'movie_reviews':movie_reviews,
-    }
-
-    return render(request,'test.html',context)
+    the_movie_details = movie_details(movie_id)
   except:
-    return HttpResponse('There seems to be no internet connection')
+    the_movie_details = 'Service not available at the moment'
+
+  try:
+    youtube_video_id = trailer_id(movie_id)
+  except:
+    youtube_video_id = 'Service not available at the moment'
+
+  try:
+    similar_movies = get_similar_movies(movie_id)
+  except:
+    similar_movies = 'Service not available at the moment'
+
+  try:
+    movie_reviews = get_movie_reviews(movie_id)
+  except:
+    movie_reviews = 'Service not available at the moment'
+
+  all_genres = []
+  production_countries = []
+  production_companies_name = []
+
+  genre = the_movie_details['genres']
+  countries = the_movie_details['production_countries']
+  companies = the_movie_details['production_companies']
+  
+  for g in genre:
+    name = g.get('name')
+    all_genres.append(name)
+
+  for p in countries:
+    name = p.get('name')
+    production_countries.append(name)
+
+  for c in companies:
+    name = c.get('name')
+    logo = c.get('logo_path')
+    origin = c.get('origin_country')
+    company_object = Company(name = name, logo = logo, origin = origin)
+    production_companies_name.append(company_object)
+
+  context = {
+
+    'youtube_video_id':youtube_video_id,
+    'the_movie_details':the_movie_details,
+    'all_genres':all_genres,
+    'production_countries':production_countries,
+    'production_companies_name':production_companies_name,
+    'similar_movies':similar_movies,
+    'movie_reviews':movie_reviews,
+  }
+
+  return render(request,'test.html',context)
+
 
 
 # SERIES
@@ -146,23 +139,21 @@ series_similar_url='https://api.themoviedb.org/3/tv/{}/similar?api_key={}'
 # Searies view function
 
 def series(request):
+  
+  top_rated_series = getSeries(series_url,'top_rated')
+  now_playing_series = getSeries(series_url,'on_the_air')
+  upcoming_series = getSeries(series_url,'airing_today')
+  popular_series = getSeries(series_url,'popular')
 
-  try:
-    top_rated_series = getSeries(series_url,'top_rated')
-    now_playing_series = getSeries(series_url,'on_the_air')
-    upcoming_series = getSeries(series_url,'airing_today')
-    popular_series = getSeries(series_url,'popular')
+  context = {
+    "top_rated_series":top_rated_series,
+    "now_playing_series":now_playing_series,
+    "upcoming_series":upcoming_series,
+    "popular_series":popular_series,
+  }
 
-    context = {
-      "top_rated_series":top_rated_series,
-      "now_playing_series":now_playing_series,
-      "upcoming_series":upcoming_series,
-      "popular_series":popular_series,
-    }
+  return render(request,'series.html',context)
 
-    return render(request,'series.html',context)
-  except:
-    return HttpResponse('There seems to be no internet connection')
 
 
 # Trailer
